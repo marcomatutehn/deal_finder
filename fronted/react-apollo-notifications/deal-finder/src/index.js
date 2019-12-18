632ed23e761c9826059c640bcde8d4aaf465f7e6
MarcoPC <marcomatute@unah.hn>
2019-12-17 22:23:42 -0600

    Service Worker Gatsby Notifications

--- fronted/react-apollo-notifications/deal-finder/src/index.js
+++
@@ -1,6 +0,0 @@
-import App from './App'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'))
--- fronted/react-apollo-notifications/src/test.ts
+++
@@ -1,7 +0,0 @@
-import { Notifications } from './'
-
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(Notifications).toBeTruthy()
-  })
-})
